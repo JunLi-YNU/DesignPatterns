b718db4d8e3ece112e83bb2eeedaf228716b66f1
JunLi_YNU <3034575826@qq.com>
2018-09-25 17:02:04 +0800

    首次提交

---
+++ src/designpatterns/Staff.java
@@ -0,0 +1,14 @@
+package designpatterns;
+
+/**
+ * 普通员工
+ * @author 12068
+ *
+ */
+public class Staff {
+	
+	public void work() {
+		//工作内容
+	}
+
+}
---
+++ src/designpatterns/VP.java
@@ -0,0 +1,12 @@
+package designpatterns;
+/**
+ * 副总裁
+ * @author 12068
+ *
+ */
+public class VP extends Staff{
+
+	public void work() {
+		//管理下面的经理
+	}
+}
